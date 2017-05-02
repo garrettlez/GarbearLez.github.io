392ec197fdee8b430d5c2e201485ebeb334146e8
Garbearlez <garbearlez@gmail.com>
2017-05-02 13:23:12 -0400

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -3,6 +3,15 @@
 
 Welcome <?php echo $_GET["name"]; ?><br>
 Your email address is: <?php echo $_GET["email"]; ?>
+  
+  <html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
 
 </body>
 </html>
